4e8fc7dcde299e0edc2df334280dbdc81839faaf
Hemant Kumar <hemant.kumar@mobileum.com>
2018-03-05 00:57:47 +0530

    Started Advanced DataStructures

---
+++ src/main/java/practice/coding/gfg/trees/binarytrees/binarysearchtrees/ConstructionAndConversion.java
@@ -0,0 +1,159 @@
+package practice.coding.gfg.trees.binarytrees.binarysearchtrees;
+
+import practice.coding.gfg.trees.binarytrees.basics.TreeNode;
+import practice.coding.gfg.trees.binarytrees.basics.TreeTraversal;
+import practice.language.java.util.DemonstrationUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// author -- hemantkumar
+public class ConstructionAndConversion {
+
+    /*
+     * -----------RED-----------------
+     *
+     * https://www.geeksforgeeks.org/construct-bst-from-given-preorder-traversa/
+     *
+     * Here the absence of the critical inorder traversal is compensated by the fact that its a Binary Search Tree. The O(n^2)
+     * algorithm is inefficient and there is an O(n) solution to the problem.
+     */
+
+    static class constructBSTFromPreOrderTraversal {
+        static int[] preTraversal = new int[]{10, 5, 1, 7, 40, 50};
+
+        constructBSTFromPreOrderTraversal(int[] preTraversal) {
+            this.preTraversal = preTraversal;
+        }
+
+        static class RecursiveVal {
+            int val;
+        }
+
+        private static TreeNode recur(int[] preTraversal, RecursiveVal elementIndex, int min, int max) {
+            // Base Case
+            if (elementIndex.val > preTraversal.length - 1) {
+                return null;
+            }
+            int element = preTraversal[elementIndex.val];
+            if (element > min && element < max) {
+                elementIndex.val = elementIndex.val + 1;
+                TreeNode treeNode = TreeNode.leafNode(element);
+                treeNode.left = recur(preTraversal, elementIndex, min, element);
+                treeNode.right = recur(preTraversal, elementIndex, element, max);
+                return treeNode;
+
+            }
+            return null;
+
+        }
+
+        public static TreeNode execute() {
+
+            RecursiveVal elementIndex = new RecursiveVal();
+            elementIndex.val = 0;
+            TreeNode root = recur(preTraversal, elementIndex, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            return root;
+
+        }
+
+        public static void demonstrate() {
+            System.out.println();
+            RecursiveVal elementIndex = new RecursiveVal();
+            elementIndex.val = 0;
+            TreeNode root = recur(preTraversal, elementIndex, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            TreeTraversal.levelOrderTraversal(root, System.out::println);
+            DemonstrationUtil.terminate();
+
+        }
+
+    }
+
+    /*------------------RED----------------------
+     *
+     * https://www.geeksforgeeks.org/binary-tree-to-binary-search-tree-conversion/
+     *
+     * It's interesting to think whether if it's even possible to convert every binary tree to a BST while preserving the structure.
+     */
+
+    /*
+     * --------------RED----------
+     *
+     * https://www.geeksforgeeks.org/sorted-linked-list-to-balanced-bst/
+     *
+     * This would have been easily implemented if it were an array. Because then accessing elements would have been O(1). But for
+     * doing this for linked list in O(1) time, a different approach needs to be used.
+     *
+     */
+
+    static class constructBSTFromSortedLinkedList {
+        public static TreeNode root = new TreeNode(null, null, 1);
+
+        public static class IncrementalVal {
+            static int val = 0;
+
+            public static void increase() {
+                val = val + 1;
+
+            }
+
+        }
+
+        public static TreeNode execute() {
+            TreeNode right = root.right;
+            TreeTraversal.recursiveInorderTraversal(root, x -> IncrementalVal.increase());
+            int nElements = IncrementalVal.val;
+            return null;
+
+        }
+
+    }
+
+    static class convertBinaryTreeToBST {
+        static TreeNode root = new TreeNode(new TreeNode(TreeNode.leafNode(8), TreeNode.leafNode(4), 2), TreeNode.leafNode(7), 10);
+
+        convertBinaryTreeToBST(TreeNode root) {
+            this.root = root;
+        }
+
+        public static TreeNode recur(ArrayList<Integer> inorderTraversal, ArrayList<Integer> sorted, TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            int val = (int) root.data;
+            int originalIndex = inorderTraversal.indexOf(val);
+            int desiredVal = sorted.get(originalIndex);
+            TreeNode newRoot = new TreeNode(null, null, desiredVal);
+            newRoot.left = recur(inorderTraversal, sorted, root.left);
+            newRoot.right = recur(inorderTraversal, sorted, root.right);
+            return newRoot;
+
+        }
+
+        public static TreeNode execute() {
+            ArrayList<Integer> inorderTraversal = new ArrayList<>();
+            TreeTraversal.recursiveInorderTraversal(root, x -> inorderTraversal.add((int) x.data));
+            ArrayList<Integer> sortedList = new ArrayList<>(inorderTraversal);
+            Collections.sort(sortedList);
+            TreeNode newRoot = recur(inorderTraversal, sortedList, root);
+            return newRoot;
+
+        }
+
+        public static void demonstrate() {
+            System.out.println("convertBinaryTreeToBST.demonstrate");
+            TreeNode newRoot = execute();
+            TreeTraversal.levelOrderTraversal(newRoot, System.out::println);
+            DemonstrationUtil.terminate();
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        constructBSTFromPreOrderTraversal.demonstrate();
+        convertBinaryTreeToBST.demonstrate();
+
+    }
+
+}
--- src/main/java/practice/coding/self/trees/TreeUtil.java
+++ src/main/java/practice/coding/self/trees/TreeUtil.java
@@ -1,67 +1,67 @@
 package practice.coding.self.trees;
 
+import practice.coding.gfg.trees.binarytrees.basics.TreeNode;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-import practice.coding.gfg.trees.TreeNode;
-
 // author -- hemantkumar
 public class TreeUtil {
 
-  // TODO: COMPLETE THE METHOD
-  public static void prettyPrintBinaryTree(TreeNode root) {
-    if (root == null) {
-      return;
-    }
-    HashMap<Integer, LinkedList<TreeNode>> levelToTreeNodesMap = new HashMap<>();
-
-    levelToTreeNodesMap.put(0, new LinkedList<TreeNode>());
-    levelToTreeNodesMap.get(0).add(root);
-    boolean moreLevels = root.left != null || root.right != null ? true : false;
-    int lastLevel = 0;
-    while (moreLevels) {
-      moreLevels = false;
-      LinkedList<TreeNode> treeNodes = levelToTreeNodesMap.get(lastLevel);
-      levelToTreeNodesMap.put(lastLevel + 1, new LinkedList<TreeNode>());
-      for (TreeNode node : treeNodes) {
-        if (node.left != null) {
-          moreLevels = true;
-          levelToTreeNodesMap.get(lastLevel + 1).add(node.left);
+    // TODO: COMPLETE THE METHOD
+    public static void prettyPrintBinaryTree(TreeNode root) {
+        if (root == null) {
+            return;
         }
-        if (node.right != null) {
-          moreLevels = true;
-          levelToTreeNodesMap.get(lastLevel + 1).add(node.right);
+        HashMap<Integer, LinkedList<TreeNode>> levelToTreeNodesMap = new HashMap<>();
+
+        levelToTreeNodesMap.put(0, new LinkedList<TreeNode>());
+        levelToTreeNodesMap.get(0).add(root);
+        boolean moreLevels = root.left != null || root.right != null ? true : false;
+        int lastLevel = 0;
+        while (moreLevels) {
+            moreLevels = false;
+            LinkedList<TreeNode> treeNodes = levelToTreeNodesMap.get(lastLevel);
+            levelToTreeNodesMap.put(lastLevel + 1, new LinkedList<TreeNode>());
+            for (TreeNode node : treeNodes) {
+                if (node.left != null) {
+                    moreLevels = true;
+                    levelToTreeNodesMap.get(lastLevel + 1).add(node.left);
+                }
+                if (node.right != null) {
+                    moreLevels = true;
+                    levelToTreeNodesMap.get(lastLevel + 1).add(node.right);
+                }
+            }
+            lastLevel++;
+            if (!moreLevels) {
+                levelToTreeNodesMap.remove(lastLevel + 1);
+            }
         }
-      }
-      lastLevel++;
-      if (!moreLevels) {
-        levelToTreeNodesMap.remove(lastLevel + 1);
-      }
-    }
 
-    HashMap<TreeNode, Integer> nodeToDescendantsCount = new HashMap<>();
-    Iterator<Integer> levelIterator = levelToTreeNodesMap.keySet().iterator();
-    while (levelIterator.hasNext()) {
-      Integer level = levelIterator.next();
-      LinkedList<TreeNode> treeNodes = levelToTreeNodesMap.get(level);
+        HashMap<TreeNode, Integer> nodeToDescendantsCount = new HashMap<>();
+        Iterator<Integer> levelIterator = levelToTreeNodesMap.keySet().iterator();
+        while (levelIterator.hasNext()) {
+            Integer level = levelIterator.next();
+            LinkedList<TreeNode> treeNodes = levelToTreeNodesMap.get(level);
+
+        }
 
     }
 
-  }
+    public static int findNodeCount(TreeNode root, HashMap<TreeNode, Integer> nodeDescendantsCount) {
 
-  public static int findNodeCount(TreeNode root, HashMap<TreeNode, Integer> nodeDescendantsCount) {
+        if (root == null) {
+            return 0;
+        }
+        if (nodeDescendantsCount.get(root) != null) {
+            return nodeDescendantsCount.get(root);
+        }
+        int nodeCount = findNodeCount(root.left, nodeDescendantsCount) + findNodeCount(root.right, nodeDescendantsCount) + 1;
+        nodeDescendantsCount.put(root, nodeCount);
+        return nodeCount;
 
-    if (root == null) {
-      return 0;
     }
-    if (nodeDescendantsCount.get(root) != null) {
-      return nodeDescendantsCount.get(root);
-    }
-    int nodeCount = findNodeCount(root.left, nodeDescendantsCount) + findNodeCount(root.right, nodeDescendantsCount) + 1;
-    nodeDescendantsCount.put(root, nodeCount);
-    return nodeCount;
-
-  }
 
 }
